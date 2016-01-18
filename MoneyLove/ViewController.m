ea72c609050e8e3d70a96e317f804abbb3b2af40
khalidmahmud <abu.khalid.mubashshir.mahmud@framgia.com>
2016-01-18 10:54:28 +0600

    Add_Transaction_Screen_Design

---
+++ MoneyLove/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  MoneyLove
+//
+//  Created by Abu Khalid on 1/15/16.
+//  Copyright © 2016 Abu Khalid. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
